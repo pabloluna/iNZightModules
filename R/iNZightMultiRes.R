3793c703e76c7e86cc419c0d061992794b014b3b
Eric Lim <jlim062@aucklanduni.ac.nz>
2015-12-21 03:19:08 +1300

    MR gui update

--- R/iNZightMultiRes.R
+++ R/iNZightMultiRes.R
@@ -1,11 +1,13 @@
 iNZightMultiRes <- setRefClass(
     "iNZightMultiRes",
     fields = list(
-        GUI         = "ANY",
-        mainGrp     = "ANY",
-        activeData = "data.frame"
+        GUI        = "ANY",
+        mainGrp    = "ANY",
+        activeData = "data.frame",
+        vars       = "character",
+        binaryVar  = "numeric"
     ),
-    
+
     methods = list(
         initialize = function(GUI) {
             g = gwindow(width = 300, height = 600)
@@ -16,99 +18,71 @@ iNZightMultiRes <- setRefClass(
             # activeData <<- tsData(dat)
             activeData <<- read.csv("//Users/eric/Desktop/CaS.csv")
             # mainGrp <<- gvbox(spacing = 10, container = GUI$moduleWindow, expand = TRUE)
-            # mainGrp$set_borderwidth(15)
+            mainGrp$set_borderwidth(2)
             top = ggroup(container = mainGrp)
-            bot = ggroup(container = mainGrp)
+            bot = glayout(container = mainGrp, expand = FALSE)
             
             # ==========
             # top panel
             # ==========
-            b  = getVars(activeData)
-            top.1 = gtable(paste("(b)", names(activeData)[b]), multiple = TRUE, container = top)
-            names(top.1) = "VARIABLES (b = binary)"
-            size(top.1)  = c(280, 280)
+            binaryVar <<- getVars(activeData)
+            vars      <<- names(activeData)
+            gtab = gtable(paste("(b)", vars[binaryVar]), multiple = TRUE, container = top)
+            names(gtab) = "VARIABLES (b = binary)"
+            size(gtab)  = c(280, 300)
             
-            addHandlerShiftclick(top.1, handler = function(h,...) {
-                cat(svalue(top.1))
-                cat("\n")
+            top.timer = NULL
+            addHandlerClicked(gtab, handler = function(h, ...) {
+                if (!is.null(top.timer))
+                    top.timer$stop_timer()
+                top.timer = gtimer(500, function(...) {
+                    updatePlot(gtab)
+                }, one.shot = TRUE)
             })
-#             addHandlerClicked(top.1, handler = function(h,...) {
-#                 cat(svalue(top.1))
-#                 cat("\n")
-#             })
-#             addHandlerControlclick(top.1, handler = function(h,...) {
-#                 cat(svalue(top.1))
-#                 cat("\n")
-#             })
-            
             
             
             # =============
             # bottom panel
             # =============
-            #bot.1 = gcombobox()
-            #bot.2 = gcombobox()
+            box1 = gcombobox(c("Select Variable 1", vars))
+            box2 = gcombobox(c("Select Variable 2", vars))
+            size(box1) = c(230, 25)
+            size(box2) = c(230, 25)
+            slider1 = gslider(0, 1)
+            slider2 = gslider(0, 1)
             
+            box1.timer = NULL
+            addHandlerChanged(box1, handler = function(h, ...) {
+                if (!is.null(top.timer))
+                    top.timer$stop_timer()
+                box1.timer = gtimer(500, function(...) {
+                    s = svalue(box1, index = TRUE) - 1
+                    if (s == 0) {
+                        slider1[] = 0:1
+                    } else {
+                        subsetVar = iNZightPlots::convert.to.factor(activeData[, s])
+                        slider1[] = 0:nlevels(subsetVar)
+                    }
+                    
+                    
+                }, one.shot = TRUE)
+            })
             
+            bot[1, 1:5, anchor = c(0,0), expand = TRUE] = box1
+            bot[3, 1:5, anchor = c(0,0), expand = TRUE] = box2
+            bot[2, 1:7, anchor = c(0,0), expand = TRUE] = slider1
+            bot[4, 1:7, anchor = c(0,0), expand = TRUE] = slider2
             
-            
-#             g1 = gframe("Select response", pos = 0.5, horizontal = FALSE, container = mainGrp)
-#             g2 = gframe("Group by", pos = 0.5, horizontal = FALSE, container = mainGrp)
-#             g3 = gframe("Group by", pos = 0.5, horizontal = FALSE, container = mainGrp)
-#             g4 = gvbox(container = mainGrp)
-#             g1$set_borderwidth(6)
-#             g2$set_borderwidth(6)
-#             g3$set_borderwidth(6)
-#             
-#             frames = getToolkitWidget(mainGrp)$getChildren()
-#             mainGrp$set_rgtk2_font(frames[[1]]$getChildren()[[2]], frameFont)
-#             mainGrp$set_rgtk2_font(frames[[2]]$getChildren()[[2]], frameFont)
-#             mainGrp$set_rgtk2_font(frames[[3]]$getChildren()[[2]], frameFont)
-#             
-#             g1_layout = glayout(container = g1)
-#             g1_opt1   = gtable(names(activeData)[getVars(activeData)[[1]]], multiple = TRUE)
-#             size(g1_opt1) = c(120, 120)
-#             g1_layout[1, 1, expand = TRUE] = g1_opt1
-#             
-#             
-#             g2_layout = glayout(container = g2)
-#             g2_opt1   = gcombobox(names(activeData)[getVars(activeData)[[1]]], multiple = TRUE)
-#             g2_opt2   = gslider(0, 2)
-#             size(g2_opt1) = c(120, 25)
-#             size(g2_opt2) = c(120, 25)
-#             
-#             g2_layout[1, 1, expand = TRUE] = g2_opt1
-#             g2_layout[2, 1, expand = TRUE] = g2_opt2
-#             
-#             g3_layout = glayout(container = g3)
-#             g3_opt1   = gcombobox(names(activeData)[getVars(activeData)[[1]]], multiple = TRUE)
-#             g3_opt2   = gslider(0, 2)
-#             size(g3_opt1) = c(120, 25)
-#             size(g3_opt2) = c(120, 25)
-#             
-#             g3_layout[1, 1, expand = TRUE] = g3_opt1
-#             g3._ayout[1, 2, exapnd = TRUE] = g3_opt2
-#             
-#             
-#             g4_layout = glayout(container = g4)
-#             plotButton = gbutton("Plot", handler = function(h,...) {
-#                 
-#             }, container = g4)
-#             summaryButton = gbutton("Summary", handler = function(h,...) {
-#                 
-#             }, container = g4)
-#             g4_layout[1, 1, expand = TRUE] = plotButton
-#             g4_layout[1, 2, expand = TRUE] = summaryButton
-            
-            
+            closeButton1 = gbutton("")
+            closeButton2 = gbutton("")
+            closeButton1$set_icon("Cancel")
+            closeButton2$set_icon("Cancel")
+            bot[1, 7, anchor = c(0,0)] = closeButton1
+            bot[3, 7, anchor = c(0,0)] = closeButton2
             
         },
         
         ## isBinary() checks for a single vector.
-        ## getVars() checks for every variable in data.
-        getVars = function(data) {
-            apply(data, 2, function(x) isBinary(x))
-        },
         isBinary = function(x) {
             ## NAs are ignored as they are handled by MR
             tab = table(x, useNA = "no")[table(x)!=0]
@@ -124,9 +98,27 @@ iNZightMultiRes <- setRefClass(
             l = grepl(re, names(tab))
             ## do BOTH binary values match the patterns?
             return(all(l))
+        },
+        ## getVars() checks for every variable in data.
+        getVars = function(data) {
+            which(apply(data, 2, function(x) isBinary(x)))
+        },
+        
+        
+        ## create an MR object and plot it
+        updatePlot = function(obj) {
+            ## create an MR object
+            clicked   = svalue(obj, index = TRUE)
+            if (length(clicked) == 1) { return() }
+            selected  = binaryVar[clicked]
+            varPieces = paste(vars[selected], collapse = " + ")
+            MRformula = as.formula(paste("mrobj", "~", varPieces))
+            MRobj1    = iNZightMR(MRformula, data = activeData, Labels = substrsplit)
+            MRobj2    = mroPara(MRobj1)
+            # MRobj2$Topic: change this or not?
+            ## plot the MR object
+            barplot(MRobj2)
         }
-        
-        
     )
 )
 
